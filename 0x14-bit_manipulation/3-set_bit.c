ebb72204fd9036fec60317f6bde3f83479c5377a
MJ4SS <jassimmoujahid72@gmail.com>
2023-10-26 14:15:09 -0700

    ff

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * set_bit - sets the value of a bit to 1 at a given index.
+ * set_bit - sets a bit at a given index to 1
+ * @n: pointer to the number to change
+ * @index: index of the bit to set to 1
  *
- * @n: address of n
- * @index: position
- * Return: 1 if it worked, or -1 if an error occurred.
+ * Return: 1 for success, -1 for failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
+	if (index > 63)
 		return (-1);
-	*n ^= (1 << index);
+
+	*n = ((1UL << index) | *n);
 	return (1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,16 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * clear_bit - sets the value of a bit to 0 at a given index.
+ * clear_bit - sets the value of a given bit to 0
+ * @n: pointer to the number to change
+ * @index: index of the bit to clear
  *
- * @n: address of n.
- * @index: position
- * Return: 1 if it worked, -1 if an error occurred.
+ * Return: 1 for success, -1 for failure
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
+	if (index > 63)
 		return (-1);
-	*n &= ~(1 << index);
+
+	*n = (~(1UL << index) & *n);
 	return (1);
 }
